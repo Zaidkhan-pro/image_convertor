cad08371732a79033b9755161b2092b27b794e9e
Zaid Khan <zkaff321@gmail.com>
2026-06-22 10:59:27 +0500

    added google search concole

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0b1326" />
+        <meta name="google-site-verification" content="2Vyv6ysWx_uCUd2zIw-QezwsoV6iguHpP8PCNdH9Wp8" />
       </head>
       <body>
         <Providers>
